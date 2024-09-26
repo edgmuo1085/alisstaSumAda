8db04f9acdc5c3ec8f4d92f72da6a4cdc44097eb
Carlos Antonio Alvarado Hernández <calvarado@ADADOM.LOCAL>
2024-09-25 19:41:31 -0500

    Agregada carpeta WWW creada desde Windows

--- www/polyfills-es5.js
+++ www/polyfills-es5.js
@@ -18875,7 +18875,7 @@ function _toPrimitive(input, hint) { if (typeof input !== "object" || input === 
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /Users/ada/Documents/apps/alisstaSumAda/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! C:\Users\calvarado\Documents\proyecto\Actual\alissta_sum\Branches\Revision_Version_Actualizada_20240920\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18883,7 +18883,7 @@ function _toPrimitive(input, hint) { if (typeof input !== "object" || input === 
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /Users/ada/Documents/apps/alisstaSumAda/src/polyfills.ts */
+    /*! C:\Users\calvarado\Documents\proyecto\Actual\alissta_sum\Branches\Revision_Version_Actualizada_20240920\src\polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
