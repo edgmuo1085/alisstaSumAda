80419de4e30a65dfcb69efabef03072fb3a0e55b
Carlos Alvarado <carlos.alvarado@ada.co>
2025-09-03 22:33:21 -0500

    advisory-topic.service.ts: Restore send activitires saveActaAsesoria() and uploadFileActaAsesoria() functions

--- src/app/services/activities/advisoryTopic/advisory-topic.service.ts
+++ src/app/services/activities/advisoryTopic/advisory-topic.service.ts
@@ -19,17 +19,17 @@ export class AdvisoryTopicService {
     const rq = actaAses;
     console.log("Save acta: ", rq);
     console.log("Save Actas Asesoria: ", rq);
-    const mockResponse: any = "true;886"; //borrar
-    return of(mockResponse); //borrar
-    // return this.http.post(this.API_SAVE_ACTA_ASESORIA, rq);
+    // const mockResponse: any = "true;886"; //borrar
+    // return of(mockResponse); //borrar
+    return this.http.post(this.API_SAVE_ACTA_ASESORIA, rq);
   }
 
   uploadFileActaAsesoria(file: UploadFile): Observable<any> {
     const rq = file;
     console.log("Upload File Acta Asesoria: ", rq);
-    const mockResponse: any = "Se envio en archivo"; //borrar
-    return of(mockResponse)//borrar
-    // return this.http.post(this.API_UPLOAD_FILE_ACTA_ASESORIA, rq);
+    // const mockResponse: any = "Se envio en archivo"; //borrar
+    // return of(mockResponse)//borrar
+    return this.http.post(this.API_UPLOAD_FILE_ACTA_ASESORIA, rq);
   }
 
   // Envio correo notificacion
